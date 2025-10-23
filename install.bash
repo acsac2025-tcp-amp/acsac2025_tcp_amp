5ce5572d8813b8c6b524a981623e3f28232c80d6
meui <976156372@qq.com>
2025-10-23 13:50:27 +0200

    update

---
+++ install.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get install build-essential -y
+sudo apt-get install -y "python$(python3 -c 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")')-venv"
+# sudo apt-get install -y iptables 
+# install corresponding python venv dep
+
+
+python3 -m venv .
+source bin/activate
+pip install numpy scapy pandas scikit-learn scipy bs4 requests matplotlib seaborn plotly
